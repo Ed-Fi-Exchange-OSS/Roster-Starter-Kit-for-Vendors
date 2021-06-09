32f5391cab9858b287983d8e79b62944ddd2bc99
Saagar Minocha <saagar.minocha93@gmail.com>
2021-06-09 14:01:46 -0500

    Added the ApiSdk folder to ChangeQueries similar to the Roster.Services project. Added ResourceTypes class to keep track of the Resourcetypes while adding records to the ChangeQueries table. Note the changes on ApiRoutes and ApiFacade (BuildResponseUri overload).

---
+++ src/EdFi.Roster.ChangeQueries/Services/ApiSdk/ResourceTypes.cs
@@ -0,0 +1,11 @@
+namespace EdFi.Roster.ChangeQueries.Services.ApiSdk
+{
+    public static class ResourceTypes
+    {
+        public static string LocalEducationAgencies => "LocalEducationAgencies";
+        public static string Schools => "Schools";
+        public static string Staff => "Staff";
+        public static string Students => "Students";
+        public static string Sections => "Sections";
+    }
+}
