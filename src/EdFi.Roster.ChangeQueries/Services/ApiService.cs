f37d2a2fa018f628d2b5a6c508feb01e4167d5f8
Suganya <suganya.rajendran@instructure.com>
2021-06-11 09:49:09 -0500

    New ApiService added for defining common method to handle DeletesResources. This can be called from different resources for handling DeletesResources

---
+++ src/EdFi.Roster.ChangeQueries/Services/ApiService.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using EdFi.Common;
+using EdFi.Roster.Models;
+using EdFi.Roster.Sdk.Client;
+using EdFi.Roster.Sdk.Models.Resources;
+
+namespace EdFi.Roster.ChangeQueries.Services
+{
+    public class ApiService
+    {
+        private readonly IResponseHandleService _responseHandleService;
+        private readonly IApiFacade _apiFacade;
+
+        public ApiService(IResponseHandleService responseHandleService
+            , IApiFacade apiFacade)
+        {
+            _responseHandleService = responseHandleService;
+            _apiFacade = apiFacade;
+        }
+
+        public async Task<ExtendedInfoResponse<List<DeletedResource>>> DeletesResources<T>(string methodName, long minVersion, long maxVersion)
+        {
+            var leaApi = await _apiFacade.GetApiClassInstance<T>();
+            var limit = 100;
+            var offset = 0;
+            var currResponseRecordCount = 0;
+            var response = new ExtendedInfoResponse<List<DeletedResource>>();
+            do
+            {
+                var errorMessage = string.Empty;
+                var responseUri = _apiFacade.BuildResponseUri($"{ApiRoutes.LocalEducationAgenciesResource}//deletes", offset, limit);
+                ApiResponse<List<DeletedResource>> currentApiResponse = null;
+                try
+                {
+                    var method = (Task<ApiResponse<List<DeletedResource>>>) typeof(T)
+                        .GetMethod(methodName)
+                        ?.Invoke(leaApi,
+                            new object[]
+                            {
+                                offset, limit, (int?) minVersion, (int?) maxVersion, default(string),
+                                default(System.Threading.CancellationToken)
+                            });
+
+                    if (method != null) currentApiResponse = await method;
+                }
+                catch (ApiException exception)
+                {
+                    errorMessage = exception.Message;
+                    if (exception.ErrorCode.Equals((int) HttpStatusCode.Unauthorized))
+                    {
+                        leaApi = await _apiFacade.GetApiClassInstance<T>(true);
+                        var method = (Task<ApiResponse<List<DeletedResource>>>) typeof(T)
+                            .GetMethod(methodName)
+                            ?.Invoke(leaApi,
+                                new object[]
+                                {
+                                    offset, limit, (int?) minVersion, (int?) maxVersion, default(string),
+                                    default(System.Threading.CancellationToken)
+                                });
+                        if (method != null) currentApiResponse = await method;
+                        errorMessage = string.Empty;
+                    }
+                }
+
+                if (currentApiResponse == null) continue;
+                currResponseRecordCount = currentApiResponse.Data.Count;
+                offset += limit;
+                response = await _responseHandleService.Handle(currentApiResponse, response, responseUri, errorMessage);
+
+            } while (currResponseRecordCount >= limit);
+
+            return response;
+        }
+    }
+}
