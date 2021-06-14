4262595ad67e9fd150805b8c218328bc636e0a66
Saagar Minocha <saagar.minocha93@gmail.com>
2021-06-14 15:28:39 -0500

    Added HasPendingChanges property to the ChangeQueryViewModel to hide/show the Sync button depending upon whether there are pending changes.

--- src/EdFi.Roster.ChangeQueries/Controllers/ChangeQueryController.cs
+++ src/EdFi.Roster.ChangeQueries/Controllers/ChangeQueryController.cs
@@ -40,12 +40,14 @@ namespace EdFi.Roster.ChangeQueries.Controllers
 
             var changeQueryModel = new ChangeQueryViewModel
             {
+                HasPendingChanges = false,
                 ChangeSummaryMessage = "No pending changes to sync. Please come back later for updates."
             };
 
 
             if (currentVersions.Count < ResourceTypes.ResourceTypeCount() || currentVersions.Select(x => x.ChangeVersion).Any(x => x < availableVersion))
             {
+                changeQueryModel.HasPendingChanges = true;
                 changeQueryModel.ChangeSummaryMessage = "There are pending changes to sync. Please click the Sync button to update your records.";
             }
 
@@ -66,6 +68,7 @@ namespace EdFi.Roster.ChangeQueries.Controllers
 
             var changeQueryModel = new ChangeQueryViewModel
             {
+                HasPendingChanges = false,
                 ChangeSummaryMessage = "Please find sync status for individual resources.",
                 SyncResponses = responses
             };
--- src/EdFi.Roster.ChangeQueries/Models/ChangeQueryViewModel.cs
+++ src/EdFi.Roster.ChangeQueries/Models/ChangeQueryViewModel.cs
@@ -4,6 +4,8 @@ namespace EdFi.Roster.ChangeQueries.Models
 {
     public class ChangeQueryViewModel
     {
+        public bool HasPendingChanges { get; set; }
+
         public string ChangeSummaryMessage { get; set; }
 
         public IEnumerable<DataSyncResponseModel> SyncResponses { get; set; }
